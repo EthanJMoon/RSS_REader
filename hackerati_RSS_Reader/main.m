087df677d09792192a221bdf77f5e8dd122b6b8e
Ethan Moon <contact@ethanmoon.com>
2014-09-08 12:00:45 -0400

    Initial Commit

---
+++ hackerati_RSS_Reader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  hackerati_RSS_Reader
+//
+//  Created by Ethan on 9/8/14.
+//  Copyright (c) 2014 em. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ hackerati_RSS_ReaderTests/hackerati_RSS_ReaderTests.m
@@ -0,0 +1,39 @@
+//
+//  hackerati_RSS_ReaderTests.m
+//  hackerati_RSS_ReaderTests
+//
+//  Created by Ethan on 9/8/14.
+//  Copyright (c) 2014 em. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hackerati_RSS_ReaderTests : XCTestCase
+
+@end
+
+@implementation hackerati_RSS_ReaderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
